6627fad72d3b54d2aafd636d14545a637eb9877c
WhiteKr <hanjisu2215g@gmail.com>
2023-12-08 23:00:20 +0900

    Refactor RPC

--- apps/front/src/utils/rpc.ts
+++ apps/front/src/utils/rpc.ts
@@ -6,9 +6,7 @@ import discordRichPresence, { PresenceInfo, RP } from 'discord-rich-presence';
  * @param {string} clientId - The client ID of the Discord Rich Presence client.
  * @returns {RP} The initialized Discord Rich Presence client.
  */
-export const initializeDiscordRPC = (clientId: string): RP => {
-  return discordRichPresence(clientId);
-};
+export const setupDiscordRPC = (clientId: string): RP => discordRichPresence(clientId);
 
 /**
  * Updates the Discord Rich Presence with the specified new state.
@@ -17,6 +15,5 @@ export const initializeDiscordRPC = (clientId: string): RP => {
  * @param {Partial<PresenceInfo>} newState - The new state to update the Rich Presence with.
  * @returns {void}
  */
-export const updateDiscordRP = (client: RP, newState: Partial<PresenceInfo>): void => {
+export const refreshDiscordRP = (client: RP, newState: Partial<PresenceInfo>): void =>
   client.updatePresence(newState);
-};
